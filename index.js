1e18d5b8f0925e3d48b77f2932a1424134ed45e7
diegozwolf <diegozuluaga88l@gmail.com>
2018-06-08 11:55:41 -0500

    create market place slide

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     //   $(window).scrollTop(checkpoint1);
     //   setTimeout(function(){
     //     scrollLock=false;
-    //   },1000) 
+    //   },1000)
     // }
     if(scroll>=checkpoint1){
       $(".navbar").addClass('navbar-better')
@@ -94,7 +94,7 @@ function changeColor(element,targetColor,property,percentage){
 //element(Jquery Element) is the element that we want to change the alpha of
 //alpha(float) is the alpha value that we want the element to take (from 0 to 1)
 //property (string) is the attribute or property what we want to change (background-color, border-color, etc.)
-function changeAlpha(element, alpha, property) { 
+function changeAlpha(element, alpha, property) {
   b = element.css(property);
   newValue= ('rgba' + b.slice(b.indexOf('('), ( (b.match(/,/g).length == 2) ? -1 : b.lastIndexOf(',') - b.length) ) + ', '+alpha+')');
   element.css(property,newValue)
@@ -102,7 +102,7 @@ function changeAlpha(element, alpha, property) {
 
 //This function gradually changes an element's attribute value depending on a percentage.
 //IT DOESN'T WORK FOR COLORS!
-//Usage: 
+//Usage:
 //element: Jquery Element - it is the element we want to change.
 //startValue: float - it is the value that we want the element to take at 0. If null it takes the value from the object
 //endValue: float - it is the value that we want the element to take at 1.
@@ -120,7 +120,3 @@ function switchElements(currentElement,targetElement,percentage){
   currentElement.css('opacity',1-percentage);
   targetElement.css('opacity',percentage);
 }
-
-
-
-
