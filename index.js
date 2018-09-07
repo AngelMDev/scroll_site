5867e267db3a355eb95ea646476e034bcaf31877
Angel <angelmmed12@gmail.com>
2018-09-07 10:36:07 -0500

    new slides

--- index.js
+++ index.js
@@ -1,141 +1,117 @@
 
 $(document).ready(function(){
   $(window).scrollTop(0);
-  var sectionHeight=parseFloat($("#basic").css("height"))
+  var sectionHeight=parseFloat($("#intro").css("height"))
   var buffer=sectionHeight*0.35;
   var buffer_short=sectionHeight*0.05
   var vh=sectionHeight-(buffer);
   var vh_short=sectionHeight-buffer_short;
 //we identify each section by these slide variables. slide0 is not defined, but it is the first one.
-  var slide1=$("#better").offset().top;
-  var slide2=$("#business").offset().top;
-  var slide3=$("#marketplace").offset().top;
-  var slide4=$("#personal").offset().top;
+  var slide2=$("#second").offset().top;
+  var slide3=$("#business").offset().top;
+  var slide4=$("#web-app").offset().top;
   var slide5=$("#mobile").offset().top;
   var slide6=$("#tablet").offset().top;
   var slide7=$("#alldevices").offset().top;
   var slide8=$("#skills").offset().top;
   var slide9=$("#contact").offset().top;
+  var currentSection=$("#intro");
   $(window).scroll(function() {
     //scroll stores the current scroll value in pixels.
     var scroll=$(window).scrollTop();
-    console.log("Scroll value",scroll)
-//==========================BASIC TO BETTER=================================
+    //console.log("Scroll value",scroll)
+//==========================INTRO TO SECOND=================================
     //check1percentage is the percentage (actually from 0 to 1) that the user has scrolled from the top of the page (plus a small buffer) to the #better section
     // 0 is at the top, 1 is when the #better section covers the whole browser window.
-    var check1percentage=(scroll-buffer)/(slide1-buffer);
-    check1percentage = Math.min(1, Math.max(0, check1percentage)); //Clamp percentage to [0 , 1]
-    if(scroll<slide1){
-      $(".navbar").removeClass('navbar-better')
-      $(".navbar-link").removeClass('btn-better')
-      $("body").removeClass('body-better')
-    }
-    if(scroll >= 0 && scroll < slide1){
-      $(".main-title-basic").removeClass("invisible");
-      $(".content-title-basic").removeClass("invisible");
-      $(".main-paragraph-basic").removeClass("invisible");
-      $(".scroll-text-basic").removeClass("invisible");
-      $(".navbar").removeClass('navbar-better');
-      changeAlpha($("#navbar-fol"),check1percentage,'background-color');
-      changeAlpha($("#navbar-fol"),1-check1percentage,'border-color');
-      switchElements($(".main-title-basic"),$(".main-title-better"),check1percentage);
-      switchElements($(".content-title-basic"),$(".content-title-better"),check1percentage);
-      switchElements($(".main-paragraph-basic"),$(".main-paragraph-better"),check1percentage);
-      switchElements($(".scroll-text-basic"),$(".scroll-text-better"),check1percentage);
-      changeAttribute($("#navbar-fol"),60,100,check1percentage,"width","%");
-      //changeColor($("#navbar-fol"),[0,0,0],'background-color',check1percentage);
-    }
-    if(scroll>=slide1){
-      $(".main-title-basic").addClass("invisible");
-      $(".content-title-basic").addClass("invisible");
-      $(".main-paragraph-basic").addClass("invisible");
-      $(".scroll-text-basic").addClass("invisible");
-      $(".navbar").addClass('navbar-better');
-      $(".navbar-link").addClass('btn-better');
-      $("body").addClass('body-better');
-      changeAlpha($("#navbar-fol"),1,'background-color');
-      changeAlpha($("#navbar-fol"),0,'border-color');
-    }
-//========================BETTER TO BUSINESS============================//
-    var check2percentage=(scroll-slide1-buffer)/(slide2-slide1-buffer);
-    check2percentage = Math.min(1, Math.max(0, check2percentage)); //Clamp percentage to [0 , 1]
+    var section1percentage=(scroll-buffer)/(slide2-buffer);
+    section1percentage = Math.min(1, Math.max(0, section1percentage)); //Clamp percentage to [0 , 1]
     if(scroll<slide2){
-
+      $(".nav-1").removeClass("is-selected")
+      $(".slide-title-second").addClass("invisible")
+      currentSection=$("#intro")
     }
-    if(scroll >= slide1 && scroll <= slide2){
-      $(".main-title-better").removeClass("invisible");
-      $(".content-title-better").removeClass("invisible");
-      $(".main-paragraph-better").removeClass("invisible");
-      $(".scroll-text-better").removeClass("invisible");
-      $("#navbar-fol").removeClass("invisible");
-      $(".scroll-down-container").removeClass("invisible");
-      $(".content-business-2").css("opacity",check2percentage);
-      $(".main-title-better").css("opacity",1-check2percentage);
-      $(".content-title-better").css("opacity",1-check2percentage);
-      $(".main-paragraph-better").css("opacity",1-check2percentage);
-      $(".scroll-text-better").css("opacity",1-check2percentage);
-      $("#navbar-fol").css("opacity",1-check2percentage);
-      $(".scroll-down-container").css("opacity",1-check2percentage);
+    if(scroll >= 0 && scroll < slide2){
+      currentSection=$("#second")
+      $(".nav-1").addClass("is-selected")
+      $(".slide-title-second").removeClass("invisible")
+      $(".slide-title-intro").removeClass("invisible")
+      switchElementsSmooth($(".slide-title-intro"),$(".slide-title-second"),section1percentage)
     }
-    if(scroll>slide2){
-      $(".main-title-better").addClass("invisible");
-      $(".content-title-better").addClass("invisible");
-      $(".main-paragraph-better").addClass("invisible");
-      $(".scroll-text-better").addClass("invisible");
-      $("#navbar-fol").addClass("invisible");
-      $(".scroll-down-container").addClass("invisible");
+    if(scroll>=slide2){
+      $(".slide-title-intro").addClass("invisible")
+      $(".nav-1").removeClass("is-selected")
     }
-
-  //==================BUSINESS TO MARKETPLACE==================//
-    var check2percentage=(scroll-slide1-buffer)/(slide2-slide1-buffer);
+//========================SECOND TO BUSINESS============================//
+    var check2percentage=(scroll-slide2-buffer)/(slide3-slide2-buffer);
     check2percentage = Math.min(1, Math.max(0, check2percentage)); //Clamp percentage to [0 , 1]
-    if(scroll<slide2){
-
+    if(scroll<slide3){
+      $(".nav-2").removeClass("is-selected")
+      $(".slide-title-business").addClass("invisible")
     }
-    if(scroll >= slide1 && scroll <= slide2){
-
+    if(scroll >= slide2 && scroll <= slide3){
+      currentSection=$("#business")
+      $(".slide-title-second").removeClass("invisible")
+      $(".slide-title-business").removeClass("invisible")
+      switchElements($('.slide-title-second'),$('.slide-title-business'),check2percentage,9,1);
+      $(".nav-2").addClass("is-selected")
     }
-    if(scroll>slide2){
-
-    }
-
-    //==================MARKETPLACE TO PERSONAL==================//
-    var check2percentage=(scroll-slide1-buffer)/(slide2-slide1-buffer);
-    check2percentage = Math.min(1, Math.max(0, check2percentage)); //Clamp percentage to [0 , 1]
-    if(scroll<slide2){
-
-    }
-    if(scroll >= slide1 && scroll <= slide2){
-
-    }
-    if(scroll>slide2){
-
+    if(scroll>slide3){
+      $(".slide-title-second").addClass("invisible")
+      $(".nav-2").removeClass("is-selected")
     }
 
-  //==================PERSONAL TO MOBILE==================//
+  // //==================BUSINESS TO WEB-APP==================//
+    var section3percentage=(scroll-slide3-buffer)/(slide4-slide3-buffer);
+    section3percentage = Math.min(1, Math.max(0, section3percentage)); //Clamp percentage to [0 , 1]
+    if(scroll<slide4){
+      $(".nav-3").removeClass("is-selected")
+      $(".slide-title-webapp").addClass("invisible")
+    }
+    if(scroll >= slide3 && scroll <= slide4){
+      currentSection=$("#web-app")
+      $(".slide-title-business").removeClass("invisible")
+      $(".slide-title-webapp").removeClass("invisible")
+      switchElementsSmooth($(".slide-title-business"),$(".slide-title-webapp"),section3percentage)
+      $(".nav-3").addClass("is-selected")
+    }
+    if(scroll>slide4){
+      $(".slide-title-business").addClass("invisible")
+      $(".nav-3").removeClass("is-selected")
+    }
+
+  //==================WEBAPP TO MOBILE==================//
     var check5percentage=(scroll-slide4-buffer)/(slide5-slide4-buffer);
     check5percentage = Math.min(1, Math.max(0, check5percentage)); //Clamp percentage to [0 , 1]
-    if(scroll<slide4){
+    if(scroll<slide5){
       $(".slide-title-mobile").addClass("invisible");
+      $(".nav-4").removeClass("is-selected")
     }
     if(scroll >= slide4 && scroll <= slide5){
+      currentSection=$("#mobile")
+      $(".slide-title-webapp").css('opacity',1-check5percentage);
       $(".slide-title-mobile").removeClass("invisible");
       $(".slide-title-mobile").css('opacity',check5percentage);
+      $(".nav-4").addClass("is-selected")
     }
     if(scroll>slide5){
-
+      $(".nav-4").removeClass("is-selected")
+      $(".slide-title-webapp").addClass("invisible")
     }
   //==================MOBILE TO TABLET==================//
     var check6percentage=(scroll-slide5-buffer)/(slide6-slide5-buffer);
-    if(scroll<slide5){
+    if(scroll<slide6){
       $(".slide-title-tablet").addClass("invisible");
+      $(".nav-5").removeClass("is-selected")
     }
     if(scroll >= slide5 && scroll <= slide6){
+      currentSection=$("#tablet")
       $(".slide-title-tablet").removeClass("invisible");
       $(".slide-title-mobile").removeClass("invisible");
       switchElementsSmooth($(".slide-title-mobile"),$(".slide-title-tablet"),check6percentage)
+      $(".nav-5").addClass("is-selected")
     }
     if(scroll>=slide6){
+      $(".nav-5").removeClass("is-selected")
       $(".slide-title-mobile").addClass("invisible");
       $(".slide-title-tablet").removeClass("invisible");
     }
@@ -145,15 +121,17 @@ $(document).ready(function(){
     check7percentage = Math.min(1, Math.max(0, check7percentage)); //Clamp percentage to [0 , 1]
     check7percentagehalfway=Math.min(1, Math.max(0, check7percentagehalfway));
     var check7percentagedelayed=(scroll-slide6-buffer*2.5)/(slide7-slide6-buffer*2.5);
-    if(scroll<slide6){
-      //
+    if(scroll<slide7){
+      $(".nav-6").removeClass("is-selected")
     }
 
-    if(scroll >= slide6 && scroll <= slide7){      
+    if(scroll >= slide6 && scroll <= slide7){  
+      currentSection=$("#alldevices")    
       switchElementsSmooth($(".slide-title-tablet"),$(".slide-title-all"),check7percentage,0,50);
+      $(".nav-6").addClass("is-selected")
     }
     if(scroll>slide7){
-      
+      $(".nav-6").removeClass("is-selected")
       $(".slide-title-skills").removeClass("invisible");
     }
     //========================ALL DEVICES TO SKILLS================//
@@ -162,17 +140,20 @@ $(document).ready(function(){
     check8percentage = Math.min(1, Math.max(0, check8percentage)); //Clamp percentage to [0 , 1]
     check8percentagehalfway=Math.min(1, Math.max(0, check8percentagehalfway));
     var check8percentagedelayed=(scroll-slide7-buffer*2.5)/(slide8-slide7-buffer*2.5);
-    if(scroll<slide7){
-      
+    if(scroll<slide8){
+      $(".nav-7").removeClass("is-selected")
     }
 
-    if(scroll >= slide7 && scroll <= slide8){      
+    if(scroll >= slide7 && scroll <= slide8){  
+      currentSection=$("#skills")      
       $(".technologies-icon-container").css("filter","grayscale("+(1-check8percentagedelayed)*100+"%)");
       switchElements($(".slide-title-all"),$(".slide-title-skills"),check8percentage,3.5,1);
       switchElements($(".slide-title-all"),$(".slide-content-skills"),check8percentage,3.5,2);
+      $(".nav-7").addClass("is-selected")
     }
-    if(scroll>slide7){    
-      //$(".slide-title-skills").removeClass("invisible");
+    if(scroll>slide8){    
+      //$(".slide-title-skills").removeClass("invisible");    
+      $(".nav-7").removeClass("is-selected")
     }
 
     // if(scroll>slide7){
@@ -185,10 +166,13 @@ $(document).ready(function(){
     //========================SKILLS TO CONTACT================//
     var check9percentage=(scroll-slide8-buffer)/(slide9-slide8-buffer);
     var check9percentagedelayed=(scroll-slide8-buffer*2.5)/(slide9-slide8-buffer*2.5);
-    if(scroll<slide8){
+    if(scroll<slide9){
       $(".slide-title-contact").addClass("invisible");
+      $(".nav-9").removeClass("is-selected")
+      $(".nav-8").removeClass("is-selected")
     }
     if(scroll >= slide8 && scroll < slide9){
+      currentSection=$("#contact")
       $(".slide-logo-contact").removeClass("logo-normal-state");
       //ifelse statement necessary because the elements would block the previous slide's icons on hover. 
       //Normally this would not be a problem since there are no elements to interact with in other slides
@@ -204,8 +188,12 @@ $(document).ready(function(){
       changeRotation($(".slide-logo-contact"),90,0,check9percentagedelayed)
       $(".slide-title-contact").css('opacity',check9percentagedelayed);
       $(".slide-logo-contact").css('opacity',check9percentagedelayed);
+      $(".nav-8").addClass("is-selected")
+      $(".nav-9").removeClass("is-selected")
     }
     if(scroll>=slide9){
+      $(".nav-8").removeClass("is-selected")
+      $(".nav-9").addClass("is-selected")
       $(".slide-logo-contact").addClass("logo-normal-state");
       $(".slide-title-contact").css('opacity',1);
       $(".slide-title-contact").removeClass("invisible");
@@ -285,8 +273,15 @@ function switchElementsSmooth(currentElement,targetElement,percentage,offsetCurr
   } 
   targetElement.css('opacity',-1+percentage*delayFactorTarget);
   if(offsetTarget!=0){
-    targetElement.css('margin-top',-moveTarget);
+    targetElement.css('margin-bottom',-moveTarget);
   }
+}
+
+function smoothScroll(target) {
+  $('body,html').animate(
+    {'scrollTop':target.offset().top+5},
+    800
+  );
 }
 
 
